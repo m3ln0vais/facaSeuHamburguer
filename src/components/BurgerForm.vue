65b643f28d6f51a2f322a6de2132e027f083caee
m3ln0vais <mellnovais14@gmail.com>
2023-08-01 01:40:27 -0300

    commit

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -109,7 +109,7 @@ export default {
   },
   methods: {
     async getIngredientes() {
-      const req = await fetch("http://localhost:3000/ingredientes");
+      const req = await fetch("https://faca-seu-hamburguer.vercel.app//ingredientes");
       const data = await req.json();
       console.log(data);
 
@@ -130,7 +130,7 @@ export default {
 
       const dataJson = JSON.stringify(data);
 
-      const req = await fetch("http://localhost:3000/burgers", {
+      const req = await fetch("https://faca-seu-hamburguer.vercel.app//burgers", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: dataJson,
